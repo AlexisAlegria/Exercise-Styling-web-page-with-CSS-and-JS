7d87ed5cb6eb3446becbedb88e4ff932c30af08e
Alexis Alegria <alexisalegriav@gmail.com>
2021-09-08 11:51:02 -0300

    Exercise: Styling web page with CSS & JS Finished

---
+++ app.js
@@ -0,0 +1,12 @@
+const paras = document.querySelectorAll('p');
+paras.forEach(p=>{
+    if(p.textContent.includes('error')){
+        p.classList.add('error');
+    } else if(p.textContent.includes('success')){
+        p.classList.add('success');
+    }
+});
+
+//Code below for learning purposes only
+const title = document.querySelector('.title');
+title.classList.toggle('test');
